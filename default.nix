6ce21fd2ffd592b4fb4e2637d844572d01f134cb
Aaron Bieber <aaron@bolddaemon.com>
2022-09-19 14:26:36 -0600

    use upstream packages

--- default.nix
+++ default.nix
@@ -1,12 +1,10 @@
 { config, pkgs, ... }:
 
-let
-  vmmClock = config.boot.kernelPackages.callPackage ./vmm_clock.nix { };
-  virtioVmmci = config.boot.kernelPackages.callPackage ./virtio_vmmci.nix { };
-in {
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+{
+  boot.kernelPackages = pkgs.linuxPackages;
   boot.kernelModules = [ "virtio_vmmci" "vmm_clock" ];
+  boot.blacklistedKernelModules = [ "intel_pmc" ];
 
   boot.kernelParams = [ "console=ttyS0,115200n8" ];
-  boot.extraModulePackages = [ virtioVmmci vmmClock ];
+  boot.extraModulePackages = with pkgs.linuxPackages; [  virtio_vmmci vmm_clock ];
 }
